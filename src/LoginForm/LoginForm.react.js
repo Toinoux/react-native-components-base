7943f59e113f301470d275fc8be22a652b379042
Antoine <antoine.yvroud@epitech.eu>
2019-03-14 11:48:46 +0100

    [WIP] Login Form tests

---
+++ src/LoginForm/LoginForm.react.js
@@ -0,0 +1,8 @@
+import { View, Text, Image, StyleSeet } from 'react-natve';
+import React, { PureComponent } from 'react';
+
+class LoginForm extends PureComponent {
+	render() {
+		return <Text>IN THE PACKAGE</Text>
+	}
+}
